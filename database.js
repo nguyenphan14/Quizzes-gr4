aa57be694397858878c3d8dad044ee380bf32f6c
phannguyen14 <chiahet2001@gmail.com>
2020-07-16 21:07:57 +0700

    Init data

---
+++ database.js
@@ -0,0 +1,47 @@
+let quizzData = [
+    {
+        name: 'Math',
+        quizzes: [
+            {
+                question: "String",
+                answer: [
+                    "string",
+                    "string",
+                    "string",
+                    "string"
+                ],
+                trueAnswer: "string"
+            }
+        ]
+    },
+    {
+        name: 'English',
+        quizzes: [
+            {
+                question: "String",
+                answer: [
+                    "string",
+                    "string",
+                    "string",
+                    "string"
+                ],
+                trueAnswer: "string"
+            }
+        ]
+    },
+    {
+        name: 'Computer',
+        quizzes: [
+            {
+                question: "String",
+                answer: [
+                    "string",
+                    "string",
+                    "string",
+                    "string"
+                ],
+                trueAnswer: "string"
+            }
+        ]
+    }
+]
