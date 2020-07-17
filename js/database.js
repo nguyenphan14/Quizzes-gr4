f9a9422ec40fbc697a012c7872187701c30caea1
phannguyen14 <chiahet2001@gmail.com>
2020-07-17 20:55:14 +0700

    Build HTML frame for begining page

---
+++ js/database.js
@@ -0,0 +1,67 @@
+let quizzData = [
+    {
+        name: 'Math',
+        quizzes: [
+            {
+                question: "String",
+                answer: [
+                    "string",
+                    "string",
+                    "string",
+                    "string"
+                ],
+                trueAnswer: "string"
+            }
+        ]
+    },
+    {
+        name: 'English',
+        quizzes: [
+            {
+                question: "String",
+                answer: [
+                    "string",
+                    "string",
+                    "string",
+                    "string"
+                ],
+                trueAnswer: "string"
+            }
+        ]
+    },
+    {
+        name: 'Computer',
+        quizzes: [
+            {
+                question: "Let's start with an easy one. What does CPU stand for?",
+                answer: [
+                    "Computer Processing Unit",
+                    "Central Peripheral Unit",
+                    "Central Processing Unit",
+                    "Computer Processing User"
+                ],
+                trueAnswer: "Central Processing Unit"
+            },
+            {
+                question: "Who invented the Graphical User Interface (GUI)?",
+                answer: [
+                    "Apple",
+                    "Bell Labs",
+                    "Xerox",
+                    "Microsoft"
+                ],
+                trueAnswer: "Xerox"
+            },
+            {
+                question: "Who invented the Graphical User Interface (GUI)?",
+                answer: [
+                    "Apple",
+                    "Bell Labs",
+                    "Xerox",
+                    "Microsoft"
+                ],
+                trueAnswer: "Xerox"
+            }
+        ]
+    }
+]
