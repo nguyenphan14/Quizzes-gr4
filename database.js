8384bbbe91c098d82fd9a4e5010690ff51fbcca8
phannguyen14 <chiahet2001@gmail.com>
2020-07-17 11:24:54 +0700

    Done IT & Computer quizzes (Nguyen)

--- database.js
+++ database.js
@@ -53,14 +53,484 @@ let quizzData = [
                 trueAnswer: "Xerox"
             },
             {
-                question: "Who invented the Graphical User Interface (GUI)?",
+                question: "Father of Free Software Foundation",
                 answer: [
-                    "Apple",
-                    "Bell Labs",
-                    "Xerox",
-                    "Microsoft"
+                    "Bill Gates",
+                    "Paul Alen",
+                    "Vint Cerf",
+                    "Richard Mathew Stallman"
                 ],
-                trueAnswer: "Xerox"
+                trueAnswer: "Richard Mathew Stallman"
+            },
+            {
+                question: "Father of Digital Camera",
+                answer: [
+                    "Steven Sasson",
+                    "David Slater",
+                    "Che Yang Lee",
+                    "William Haken Lee"
+                ],
+                trueAnswer: "Steven Sasson"
+            },
+            {
+                question: "Father of Internet",
+                answer: [
+                    "Tim Bernerslee",
+                    "Ray Tomlison",
+                    "Bill Gates",
+                    "Vint Cerf"
+                ],
+                trueAnswer: "Vint Cerf"
+            },
+            {
+                question: "Father of FM Broadcasting",
+                answer: [
+                    "William Latham Hughes",
+                    "Thomas Alva Edison",
+                    "Michel Farade",
+                    "MicrosEdwin Howard Armstrongoft"
+                ],
+                trueAnswer: "Edwin Howard Armstrong"
+            },
+            {
+                question: "Father of Super Computer",
+                answer: [
+                    "John McCarthy",
+                    "Seymour Cray",
+                    "Sanjay Bhatia",
+                    "Steve Jobs"
+                ],
+                trueAnswer: "Seymour Cray"
+            },
+            {
+                question: "Who is heading IndiaTech, the lobby group formed by Flipkart,ola,MMT...?",
+                answer: [
+                    "Binny Bansal",
+                    "Ashish Kashyap",
+                    "Deep Kalra",
+                    "Gyanendra Badgaiyan"
+                ],
+                trueAnswer: "Gyanendra Badgaiyan"
+            },
+            {
+                question: "YouTube has launched its first ever pop-up space in which city?",
+                answer: [
+                    "New Delhi",
+                    "Bangalore",
+                    "Hyderabad",
+                    "Mumbai"
+                ],
+                trueAnswer: "Hyderabad"
+            },
+            {
+                question: "The first high-level programming was?",
+                answer: [
+                    "FORTRAN",
+                    "LISP",
+                    "COBOL",
+                    "Pascal"
+                ],
+                trueAnswer: "FORTRAN"
+            },
+            {
+                question: "The First algorithmic programming language was ……",
+                answer: [
+                    "Basic",
+                    "C",
+                    "Plankalkuel",
+                    "Fortran"
+                ],
+                trueAnswer: "Plankalkuel"
+            },
+            {
+                question: "The first ever language used on an electronic computing device is",
+                answer: [
+                    "LISP",
+                    "Prolog",
+                    "Short Code",
+                    "C"
+                ],
+                trueAnswer: "Short Code"
+            },
+            {
+                question: "The first object oriented programming language is….",
+                answer: [
+                    "SIMULA 67",
+                    "Python",
+                    "Smalltalk",
+                    "Ruby"
+                ],
+                trueAnswer: "SIMULA 67"
+            },
+            {
+                question: "The first Block structured language is……",
+                answer: [
+                    "ALGOL 60",
+                    "C++",
+                    "C",
+                    "FORTRAN"
+                ],
+                trueAnswer: "ALGOL 60"
+            },
+            {
+                question: "The first artificial intelligance (AI) program is………",
+                answer: [
+                    "LISP",
+                    "Python",
+                    "BASIC",
+                    "C++"
+                ],
+                trueAnswer: "LISP"
+            },
+            {
+                question: "The first operating system for computer was……..",
+                answer: [
+                    "386BSD",
+                    "CP/M",
+                    "AmigaOS",
+                    "Digital UNIX"
+                ],
+                trueAnswer: "CP/M"
+            },
+            {
+                question: "Among the following, which term is related to memory?",
+                answer: [
+                    "WAN",
+                    "MAN",
+                    "CRT",
+                    "RAM"
+                ],
+                trueAnswer: "RAM"
+            },
+            {
+                question: "BASIC, C and COBOL and Java are examples of which language? ",
+                answer: [
+                    "Computer language",
+                    "Low level language",
+                    "System programming language",
+                    "High-level language"
+                ],
+                trueAnswer: "Low level language"
+            },
+            {
+                question: " Which one of the following terms refer to the most popular LAN technology",
+                answer: [
+                    "Ethernet",
+                    "WAN",
+                    "Internet",
+                    "Intranet"
+                ],
+                trueAnswer: "Ethernet"
+            },
+            {
+                question: "A computer network is a system of:",
+                answer: [
+                    "two or more computing devices connected to one another",
+                    "Data transfer and information storage",
+                    "Sharing devices like scanners",
+                    "A whole lot of individual machines working on their own"
+                ],
+                trueAnswer: "two or more computing devices connected to one another"
+            },
+            {
+                question: " Hardware needed to connect to a computer to the internet is:",
+                answer: [
+                    "NIC",
+                    "Internet Explorer",
+                    "Servers",
+                    "Mouse"
+                ],
+                trueAnswer: "NIC"
+            },
+            {
+                question: "The acronym for PC stands for",
+                answer: [
+                    "Personal Computer",
+                    "Private Computer",
+                    "Personal Compact",
+                    "Privacy Computer"
+                ],
+                trueAnswer: "Personal Computer"
+            },
+            {
+                question: "This computer is a multiprocessing computer system that is smaller and slower than a mainframe.",
+                answer: [
+                    "Supercomputer",
+                    "Personal Computer",
+                    "Minicomputer",
+                    "Embedded Computer"
+                ],
+                trueAnswer: "Personal Computer"
+            },
+            {
+                question: "Complex scientific research is usually done by",
+                answer: [
+                    "Mainframe computer",
+                    "Minicomputer",
+                    "Microcomputer",
+                    "Supercomputer"
+                ],
+                trueAnswer: "Supercomputer"
+            },
+            {
+                question: "Choose the best answer that defines information technology.",
+                answer: [
+                    "The use of hardware and software to store, retrieve and manipulate information.",
+                    "Sharing personal idea, photos and videos with others.",
+                    "Involve having a current knowledge and understanding of computer.",
+                    "To enable us to play games or access other sources of information."
+                ],
+                trueAnswer: "The use of hardware and software to store, retrieve and manipulate information."
+            },
+            {
+                question: "Which one of the following terms refer to wireless radio communication?",
+                answer: [
+                    "Bluetooth",
+                    "Bandwidth",
+                    "GPS",
+                    "Ethernet"
+                ],
+                trueAnswer: "Bluetooth"
+            },
+            {
+                question: "One of the advantages of Information Technology is",
+                answer: [
+                    "Speed and power",
+                    "Strength and power",
+                    "Speed and Strength",
+                    "Speed and accuracy"
+                ],
+                trueAnswer: "Speed and accuracy"
+            },
+            {
+                question: "The fastest supercomputer nowadays is",
+                answer: [
+                    "Cray Titan",
+                    "Cray Jaguar",
+                    "Sunway TaihuLight",
+                    "NUDT-Tianhe-2"
+                ],
+                trueAnswer: "Sunway TaihuLight"
+            },
+            {
+                question: "One of the applications of IT is",
+                answer: [
+                    "E-perolehan",
+                    "Maybank2u",
+                    "Global Positioning System",
+                    "Travel"
+                ],
+                trueAnswer: "Travel"
+            },
+            {
+                question: "One of the disadvantages of Information Technology is",
+                answer: [
+                    "High risk",
+                    "Deep impact",
+                    "Impact on labour force",
+                    "Impact on population"
+                ],
+                trueAnswer: "Impact on labour force"
+            },
+            {
+                question: "Learning Management System is an application of IT in",
+                answer: [
+                    "Finance",
+                    "Web blogging",
+                    "Web Based Learning",
+                    "Education"
+                ],
+                trueAnswer: "Education"
+            },
+            {
+                question: "IPad comes from one of this category of computer.",
+                answer: [
+                    "Mobile computer",
+                    "Mobile phone",
+                    "Mobile tablet",
+                    "Embedded computer"
+                ],
+                trueAnswer: "Mobile computer"
+            },
+            {
+                question: "These are examples of IT application in finance.",
+                answer: [
+                    "Online ticketing and GPS",
+                    "Online banking and EPS",
+                    "Online storage and RPS",
+                    "Online gaming and FPS"
+                ],
+                trueAnswer: "Online banking and EPS"
+            },
+            {
+                question: "One of the applications of Information Technology is",
+                answer: [
+                    "Health issue",
+                    "Health problem",
+                    "Health care",
+                    "Health smart"
+                ],
+                trueAnswer: "Health care"
+            },
+            {
+                question: "One of the characteristics of mobile computer is",
+                answer: [
+                    "big",
+                    "fast",
+                    "teleport",
+                    "portable"
+                ],
+                trueAnswer: "portable"
+            },
+            {
+                question: "Consumer electronics and home automation system are examples of what category of computer?",
+                answer: [
+                    "Mobile device",
+                    "Embedded computer",
+                    "Mobile computer",
+                    "Mini computer"
+                ],
+                trueAnswer: "Embedded computer"
+            },
+            {
+                question: " Mainframe can also act as a ________ in a network.",
+                answer: [
+                    "Manager",
+                    "Server",
+                    "Client",
+                    "Helper"
+                ],
+                trueAnswer: "Server"
+            },
+            {
+                question: "One of the examples of IT application in manufacturing is CAM. What does CAM stands for?",
+                answer: [
+                    "Camera and monitor",
+                    "Computer and monitor",
+                    "Computer aided maturity",
+                    "Computer aided manufacturing"
+                ],
+                trueAnswer: "Computer aided manufacturing"
+            },
+            {
+                question: "Cochlea implant allows a deaf person to listen. What branch of applications in Information Technology does it come from?",
+                answer: [
+                    "Health",
+                    "Science",
+                    "Manufacturing",
+                    "Education"
+                ],
+                trueAnswer: "Science"
+            },
+            {
+                question: "All of these are applications in Information Technology except",
+                answer: [
+                    "Manufacturing",
+                    "Education",
+                    "Finance",
+                    "Internet"
+                ],
+                trueAnswer: "Internet"
+            },
+            {
+                question: "Which one of the following terms refer to wireless radio communication?",
+                answer: [
+                    "Bandwidth",
+                    "GPS",
+                    "Ethernet",
+                    "Bluetooth"
+                ],
+                trueAnswer: "Bluetooth"
+            },
+            {
+                question: "Two examples of application of IT in government are",
+                answer: [
+                    "e-hasil and e-visitor",
+                    "e-perolehan and e-penyata",
+                    "e-government and SPP III",
+                    "e-syariah and e-filling"
+                ],
+                trueAnswer: "e-perolehan and e-penyata"
+            },
+            {
+                question: "You can read newspaper online which is one of the applications of Information Technology.",
+                answer: [
+                    "e-book",
+                    "online newspaper",
+                    "online blog",
+                    "publishing"
+                ],
+                trueAnswer: "publishing"
+            },
+            {
+                question: "If you want to visit a country such as Russia, you have got to use one of these application in Information Technolgy.",
+                answer: [
+                    "Finance",
+                    "Education",
+                    "Publishing",
+                    "Traveling"
+                ],
+                trueAnswer: "Traveling"
+            },
+            {
+                question: " Which of these is Not an early computer?",
+                answer: [
+                    "NASA",
+                    "SAGE",
+                    "ENIAC",
+                    "UNIVAC"
+                ],
+                trueAnswer: "NASA"
+            },
+            {
+                question: "How many computer languages are in use?",
+                answer: [
+                    "20",
+                    "50",
+                    "2000",
+                    "5000"
+                ],
+                trueAnswer: "2000"
+            },
+            {
+                question: "Which of the following is NOT one of the early \"protocols,\" or ways to use the Internet?",
+                answer: [
+                    "Gopher",
+                    "FTP",
+                    "Telnet",
+                    "blogging"
+                ],
+                trueAnswer: "blogging"
+            },
+            {
+                question: " What does the Internet prefix WWW stand for?",
+                answer: [
+                    "Word Wide Web",
+                    "Western Washington World",
+                    "Worldwide Weather",
+                    "Wide Width Wickets"
+                ],
+                trueAnswer: "Word Wide Web"
+            },
+            {
+                question: "A network designed to allow communication within an organization is called:",
+                answer: [
+                    "World Wide Web",
+                    "intranet",
+                    "Internet",
+                    "Yahoo"
+                ],
+                trueAnswer: "intranet"
+            },
+            {
+                question: "Which of the following would be the most reliable source of information for a school report?  ",
+                answer: [
+                    "Political blog or radio show.",
+                    "Wikipedia",
+                    "www.nationalgeographic.com",
+                    "None"
+                ],
+                trueAnswer: "www.nationalgeographic.com"
             }
         ]
     }
